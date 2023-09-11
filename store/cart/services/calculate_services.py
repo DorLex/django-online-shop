1096c3713597be1db8a16ab6ccdba11ee707797e
DorLex <dorozhinskiysanya@gmail.com>
2023-09-11 21:48:26 +0300

    add operations with F obj

--- store/cart/services/calculate_services.py
+++ store/cart/services/calculate_services.py
@@ -1,3 +1,6 @@
+from django.db.models import F
+
+
 def calculate_several_price(product, product_quantity):
     """Считает стоимость нескольких одинаковых товаров"""
     several_price = product.price * int(product_quantity)
@@ -6,14 +9,14 @@ def calculate_several_price(product, product_quantity):
 
 def increase_cart_product_quantity(cart_product, product_quantity):
     """Прибавляет количество одинаковых товаров в корзине"""
-    cart_product.quantity += int(product_quantity)
+    cart_product.quantity = F('quantity') + int(product_quantity)
 
 
 def increase_cart_product_several_price(cart_product, several_price):
     """Прибавляет стоимость одинаковых товаров в корзине"""
-    cart_product.several_price += several_price
+    cart_product.several_price = F('several_price') + int(several_price)
 
 
 def update_cart_total_price(user_cart, value):
     """Обновляет общую стоимость корзины"""
-    user_cart.total_price += value
+    user_cart.total_price = F('total_price') + int(value)
